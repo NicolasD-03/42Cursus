05998962a3709a6d0945ce1b05a5f75237058d41
Nicolas Dick <ndick@k1r1p14.42mulhouse.fr>
2022-11-24 21:52:41 +0100

    Feat : Finish

--- libft/ft_lstmap.c
+++ libft/ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: ndick <ndick@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/17 00:59:00 by ndick             #+#    #+#             */
-/*   Updated: 2022/11/24 19:40:24 by ndick            ###   ########.fr       */
+/*   Updated: 2022/11/24 21:38:06 by ndick            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,17 +16,19 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
 	t_list	*new;
 	t_list	*begin;
+	void	*content;
 
 	if (!f || !del || !lst)
 		return (NULL);
 	begin = NULL;
 	while (lst)
 	{
-		new = ft_lstnew(f(lst->content));
+		content = f(lst->content);
+		new = ft_lstnew(content);
 		if (!new)
 		{
-			ft_lstdelone(new, del);
 			ft_lstclear(&begin, del);
+			del(content);
 			return (NULL);
 		}
 		ft_lstadd_back(&begin, new);
